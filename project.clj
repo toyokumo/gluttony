9eabc17b4374d453ea2dca9dafd9a6e757dfee95
makinoshi <masa.kinoshita02@gmail.com>
2020-04-01 15:15:03 +0900

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toyokumo/gluttony "0.1.0"
+(defproject toyokumo/gluttony "0.1.1-SNAPSHOT"
   :description "A consumer library using core.async and aws-api based on AWS SQS"
   :url "https://github.com/toyokumo/gluttony"
   :license {:name "Apache, Version 2.0"
